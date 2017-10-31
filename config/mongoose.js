dbd26eecb3502c2dad552fdfd11adb42fd936aab
moeabdol <mohd.a.saed@gmail.com>
2017-11-01 00:46:19 +0300

    Change mongoose promise to avoid deprecation warning

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const config = require('./config');
 
+mongoose.Promise = global.Promise;
+
 mongoose.connect(config.db, { useMongoClient: true })
   .then(() => console.log('Connected to', config.db))
   .catch(err => console.log(err));
