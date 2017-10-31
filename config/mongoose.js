750338dee0f5048069693567c8cc0061ff214180
moeabdol <mohd.a.saed@gmail.com>
2017-11-01 00:45:33 +0300

    Configure mongoose and add config

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,1 +1,7 @@
 const mongoose = require('mongoose');
+
+const config = require('./config');
+
+mongoose.connect(config.db, { useMongoClient: true })
+  .then(() => console.log('Connected to', config.db))
+  .catch(err => console.log(err));
