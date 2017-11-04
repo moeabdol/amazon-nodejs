cba952ea5faeff3a3c3f651a9070e05d005de3ad
moeabdol <mohd.a.saed@gmail.com>
2017-11-04 23:16:41 +0300

    Append ajax search results to product-main view

--- public/js/main.js
+++ public/js/main.js
@@ -12,7 +12,22 @@ $(document).ready(function() {
           return hit;
         });
 
-        console.log(data);
+        $('#search-results').empty();
+
+        for (var i = 0; i < data.length; i++) {
+          var html = '';
+          html += '<div class="col-md-4">';
+          html += '<a href="/product/' + data[i]._source._id + '">';
+          html += '<div class="card mb-3">';
+          html += '<img class="card-img-top" src="' + data[i]._source.image + '" alt="">';
+          html += '<div class="card-body">';
+          html += '<h4 class="card-title">' + data[i]._source.name + '</h4>';
+          html += '<h4 class="card-text">' + data[i]._source.category.name + '</h4>';
+          html += '<h4 class="card-text">' + data[i]._source.price + '</h4>';
+          html +='</div></div></a></div>';
+
+          $('#search-results').append(html);
+        }
       },
       error: function(err) {
         console.log(err);
